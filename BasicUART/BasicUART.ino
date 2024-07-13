167d6667afc420c84f43b761c443131ce696e5a0
RobinAdam747 <adam.sendzul@gmail.com>
2024-07-13 16:59:44 +0200

    Update BasicUART.ino
    switched RX and TX

--- BasicUART/BasicUART.ino
+++ BasicUART/BasicUART.ino
@@ -1,12 +1,14 @@
 // Code found at: https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/uart.html and https://www.youtube.com/watch?v=TGusjcKSNIU
 
-// #define RXp2 16
-// #define TXp2 17
+#define RXuart 16  // RX pin connected to WellPro
+#define TXuart 17  // TX pin connected to WellPro
 
 void setup() {
   // put your setup code here, to run once:
-  // Serial.begin(115200);
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RXuart, TXuart);
+
+  Serial.println("Setup complete");
 
   // UART setup
   // const uart_port_t uart_num = UART_NUM_2;
@@ -25,6 +27,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  Serial.println("Transmitting request...");
+  //Serial.write(69);
+  Serial2.write(1);
+  delay(1000);
 
   // Serial.println("Data Received: " + Serial.readString());
 
