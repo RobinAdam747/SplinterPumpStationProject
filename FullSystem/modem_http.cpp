5cf44eedc7fba9373d462c940a884b66e19766df
Alex <bergesenalex@gmail.com>
2024-08-08 20:29:58 +0200

    Update modem_http.cpp

--- FullSystem/modem_http.cpp
+++ FullSystem/modem_http.cpp
@@ -10,74 +10,60 @@ void initialiseModem() {
   Serial2.print("usr.cn#AT+CGDCONT=1,\"IP\",\"");
   Serial2.print(modemAPN);
   Serial2.println("\"");
-  if (!waitForResponse("OK")) return;
+  delay(200);
 
   // Connect to network
   Serial2.println("usr.cn#AT+CGATT=1");
-  if (!waitForResponse("OK")) return;
+  delay(200);
 
   // Set up HTTP mode
   Serial2.println("usr.cn#AT+HTTPINIT");
-  if (!waitForResponse("OK")) return;
+  delay(200);
 }
 
 void sendHttpPost() {
   // Set HTTP parameters
   Serial2.println("usr.cn#AT+HTTPPARA=\"CID\",1");
-  waitForResponse("OK");
+  delay(200);
 
   Serial2.print("usr.cn#AT+HTTPPARA=\"URL\",\"");
   Serial2.print(url);
   Serial2.println("\"");
-  waitForResponse("OK");
+  delay(200);
 
   Serial2.print("usr.cn#AT+HTTPPARA=\"USERDATA\",\"Authorization: ");
   Serial2.print(token);
   Serial2.println("\"");
-  waitForResponse("OK");
+  delay(200);
 
   Serial2.print("usr.cn#AT+HTTPPARA=\"CONTENT\",\"");
   Serial2.print(contentType);
   Serial2.println("\"");
-  waitForResponse("OK");
+  delay(200);
 
   // Set data length
   Serial2.print("usr.cn#AT+HTTPDATA=");
   Serial2.print(strlen(jsonData));
   Serial2.println(",10000");
-  if (!waitForResponse("DOWNLOAD")) return;
+  delay(200);
 
   // Send JSON data
   Serial2.print(jsonData);
-  if (!waitForResponse("OK")) return;
+  delay(200);
 
   // Start HTTP POST
   Serial2.println("usr.cn#AT+HTTPACTION=1");
-  if (!waitForResponse("OK")) return;
+  delay(200);
 
   // Clean up HTTP session
   // Terminate HTTP session
   Serial2.println("usr.cn#AT+HTTPTERM");
-  waitForResponse("OK");
+  delay(200);
 
   // Switch modem back to MQTT mode
   Serial2.println("usr.cn#AT+MODE=MQTT");
-  waitForResponse("OK");
+  delay(200);
 }
-
-bool waitForResponse(const char* expectedResponse) {
-  unsigned long timeout = millis() + 5000;  // 5 seconds timeout
-  while (millis() < timeout) {
-    if (Serial2.available()) {
-      String response = Serial2.readString();
-      if (response.indexOf(expectedResponse) != -1) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
 
 void checkThresholds() {
   for (int i = 0; i < 8; ++i) {
