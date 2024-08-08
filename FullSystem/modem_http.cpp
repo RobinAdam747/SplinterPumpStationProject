bea82b1b1e1011651706e702e8b5b9f59bfb699a
Alex <bergesenalex@gmail.com>
2024-08-08 20:40:54 +0200

    Update modem_http.cpp

--- FullSystem/modem_http.cpp
+++ FullSystem/modem_http.cpp
@@ -9,42 +9,42 @@ void initialiseModem() {
   // Set APN
   Serial2.print("usr.cn#AT+CGDCONT=1,\"IP\",\"");
   Serial2.print(modemAPN);
-  Serial2.println("\"");
+  Serial2.println("\"\r");
   delay(200);
 
   // Connect to network
-  Serial2.println("usr.cn#AT+CGATT=1");
+  Serial2.println("usr.cn#AT+CGATT=1\r");
   delay(200);
 
   // Set up HTTP mode
-  Serial2.println("usr.cn#AT+HTTPINIT");
+  Serial2.println("usr.cn#AT+HTTPINIT\r");
   delay(200);
 }
 
 void sendHttpPost() {
   // Set HTTP parameters
-  Serial2.println("usr.cn#AT+HTTPPARA=\"CID\",1");
+  Serial2.println("usr.cn#AT+HTTPPARA=\"CID\",1\r");
   delay(200);
 
   Serial2.print("usr.cn#AT+HTTPPARA=\"URL\",\"");
   Serial2.print(url);
-  Serial2.println("\"");
+  Serial2.println("\"\r");
   delay(200);
 
   Serial2.print("usr.cn#AT+HTTPPARA=\"USERDATA\",\"Authorization: ");
   Serial2.print(token);
-  Serial2.println("\"");
+  Serial2.println("\"\r");
   delay(200);
 
   Serial2.print("usr.cn#AT+HTTPPARA=\"CONTENT\",\"");
   Serial2.print(contentType);
-  Serial2.println("\"");
+  Serial2.println("\"\\r");
   delay(200);
 
   // Set data length
   Serial2.print("usr.cn#AT+HTTPDATA=");
   Serial2.print(strlen(jsonData));
-  Serial2.println(",10000");
+  Serial2.println(",10000\r");
   delay(200);
 
   // Send JSON data
@@ -52,16 +52,16 @@ void sendHttpPost() {
   delay(200);
 
   // Start HTTP POST
-  Serial2.println("usr.cn#AT+HTTPACTION=1");
+  Serial2.println("usr.cn#AT+HTTPACTION=1\r");
   delay(200);
 
   // Clean up HTTP session
   // Terminate HTTP session
-  Serial2.println("usr.cn#AT+HTTPTERM");
+  Serial2.println("usr.cn#AT+HTTPTERM\r");
   delay(200);
 
   // Switch modem back to MQTT mode
-  Serial2.println("usr.cn#AT+MODE=MQTT");
+  Serial2.println("usr.cn#AT+MODE=MQTT\r");
   delay(200);
 }
 
